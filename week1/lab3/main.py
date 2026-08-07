ba64a7145be8b1c328bdb2b131c0a7af07e85e18
palak200526 <palakgrover2611@gmail.com>
2026-08-07 10:25:52 +0530

    feat: add type hints and reusable helper function

--- week1/lab3/main.py
+++ week1/lab3/main.py
@@ -1,12 +1,17 @@
 import string
+from typing import List, Dict
 from collections import Counter
 
-# This function takes a string as input and returns a dictionary of word to count and convert the string to lowercase and remove punctuation
-def word_count(text):
+def word_list(text: str) -> list:
     text= text.lower()
     for ch in string.punctuation:
         text = text.replace(ch, "")
     words= text.split()
+    return words
+
+# This function takes a string as input and returns a dictionary of word to count and convert the string to lowercase and remove punctuation
+def word_count(text: str) -> Dict[str, int]:
+    words= word_list(text)
     counts = {}
 
     for word in words:
@@ -16,18 +21,15 @@ def word_count(text):
 #print(word_count("Hello, Hello World!"))
 
 # This fuction also do the same thing as word_count but uses the Counter class from the collections module to count the words in the string
-def word_count_counter(text):
-    text= text.lower()
-    for ch in string.punctuation:
-        text = text.replace(ch, "")
-    words= text.split()
+def word_count_counter(text: str) -> Counter:
+    words= word_list(text)
     counts = Counter(words)
 
     return counts
 #print(word_count_counter("Hello, Hello World!"))
 
 # This function takes a list of lists as input and returns a flattened list using a loop
-def flatten_loop(list_of_lists):
+def flatten_loop(list_of_lists: List[List[int]]) -> List[int]:
     result = []
 
     for sublist in list_of_lists:
@@ -38,7 +40,7 @@ def flatten_loop(list_of_lists):
 #print(flatten_loop([[1,2],[3,4],[5]]))
 
 # This function takes a list of lists as input and returns a flattened list using a list comprehension
-def flatten_comp(list_of_lists):
+def flatten_comp(list_of_lists: List[List[int]]) -> List[int]:
     return [item for sublist in list_of_lists for item in sublist]
 #print(flatten_comp([[1,2],[3,4],[5]]))
 
